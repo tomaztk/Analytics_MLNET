1cb86bf3deef33396c06ff91efbb2dca6993e10f
tomaztk <tomaz.kastrun@gmail.com>
2020-09-30 09:36:40 +0200

    initial commit

---
+++ TaxiFarePrediction/TestTrips.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxiFarePrediction
+{
+    // <Snippet1>
+    static class TestTrips
+    // </Snippet1>
+    {
+        // <Snippet2>
+        internal static readonly TaxiTrip Trip1 = new TaxiTrip
+        {
+            VendorId = "VTS",
+            RateCode = "1",
+            PassengerCount = 1,
+            TripDistance = 6.34f,
+            PaymentType = "CRD",
+            FareAmount = 0 // predict it. actual = 29.5
+        };
+        // </Snippet2>
+    }
+}
+
+
+
+
+
+
